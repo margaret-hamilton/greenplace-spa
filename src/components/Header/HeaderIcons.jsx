fe5bd33e36c988305e5d48e84b892e314c460539
Vinicius Ribeiro <vinicius.vrf@gmail.com>
2022-05-07 20:26:52 -0300

    ✨ 💄 Update UI

--- src/components/Header/HeaderIcons.jsx
+++ src/components/Header/HeaderIcons.jsx
@@ -4,10 +4,10 @@ export function HeaderIcons() {
   return (
     <div class="order-lg-last flex-shrink-0">
       <div className="widget-header  ms-2">
-        <button className="btn btn-icon btn-light icon icon-sm rounded-circle bg-light">
+        <a href="/#" className="icon icon-sm rounded-circle">
           <i className="fa fa-shopping-cart"></i>
           <span className="notify">0</span>
-        </button>
+        </a>
       </div>
     </div>
   );
